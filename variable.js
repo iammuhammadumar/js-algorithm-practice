4c7294cf8da791f02612e400445ebab05a0dca26
Muhammad Umar <muhammadumar@Muhammads-MacBook-Pro.local>
2022-10-03 04:04:53 +0500

    array mutations

--- variable.js
+++ variable.js
@@ -72,8 +72,31 @@ newGuestList.forEach(name => {
     console.log('newGuestList is', name);
 });
 
+/*
+Shrinking Guest List: You just found out that your new dinner table
+won’t arrive in time for the dinner, and you have space for only two guests.
+• Start with your program from Exercise 16.
+Add a new line that prints a message saying that you can invite only two people for dinner.
 
+• Remove guests from your list one at a time until
+ only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
 
-let finalGuestList = newGuestList.splice(1, newGuestList.length - 5);
+• Print a message to each of the two people still on your list, letting them know they’re still invited.
+
+• Remove the last two names from your list, so you
+ have an empty list. Print your list to make sure you actually have an empty list at the end of your program.
+*/
+console.log('Prob 17');
+
+let finalGuestList = newGuestList.filter((item, index) => index === 0 || index === 1)
 
 console.log('finalGuestList is', finalGuestList.length);
+
+finalGuestList.map(name => {
+    console.log('you are still invited', name);
+    finalGuestList.shift(name);
+});
+
+finalGuestList.splice(0, 2);
+
+console.log('Empty list: ', finalGuestList.length);
